31a422c441aab0d5d54e218ba1a00e892b3ffbca
Dan Kelpsch <kelpschd@gmail.com>
2026-06-13 19:24:53 +0000

    fixes to pyramid omezarr printing

--- src/Import_export.py
+++ src/Import_export.py
@@ -5,7 +5,7 @@ import ome_zarr
 from ome_zarr.io import parse_url
 from ome_zarr.writer import write_image
 
-# set this up in a loop
+# set this up in a loop and to take in inputs from bash
 images_dir = Path("/mnt/efs/dl_jrc/student_data/S-DK/Sphere/220725_i11w-hT-M33-I76_sg1035_d10sphere")
 images = list(sorted(images_dir.glob('*.nd2')))
 print(f"Found {len(images)} ND2 files")
@@ -20,6 +20,8 @@ root = zarr.group(store, overwrite=True)
 with nd2.ND2File(nd2_path) as f:
     arr = f.asarray()
     print(f"Shape: {arr.shape}")
+
+    # pixel conversion information - verify this is the correct value
     px = f.metadata.channels[0].volume.axesCalibration
     print(px)
     
@@ -27,14 +29,10 @@ with nd2.ND2File(nd2_path) as f:
         image=arr,
         group=root,
         axes="tcyx",
-        n_lvls=3,  # stop at 512x512
+        scale_factors={}, 
         coordinate_transformations=[
-            [{"type": "scale", "scale": [1.0, 1.0, px[0] * (2**i), px[1] * (2**i)]}]
-            for i in range(5)
+            [{"type": "scale", "scale": [1.0, 1.0, px[0], px[1]]}]
         ],
     )
+
     print("Zarr'd")
-
-
-# set up a definition to take a nd2 file (using nd2 package)
-# nd2 directly to zarr - inbetween step with np?
