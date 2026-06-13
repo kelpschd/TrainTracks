d3acf8221827046f7c7e816f7ffe58cd7e371299
Dan Kelpsch <kelpschd@gmail.com>
2026-06-13 20:28:42 +0000

    fix to put zarr in same folder as images_dir

--- src/Import_export.py
+++ src/Import_export.py
@@ -11,38 +11,34 @@ images_dir = Path("/mnt/efs/dl_jrc/student_data/S-DK/Sphere/220725_i11w-hT-M33-I
 images = list(sorted(images_dir.glob('*.nd2')))
 print(f"Found {len(images)} ND2 files")
 
-nd2_path = images[0]
-zarr_path = nd2_path.with_suffix('.zarr').name
-
-store = zarr.storage.LocalStore(zarr_path) 
-root = zarr.group(store, overwrite=True)
-
-with nd2.ND2File(nd2_path) as f:
-    arr = f.asarray()
+for image in images:
+    zarr_fn = images[0].with_suffix('.zarr').name
+    print(zarr_fn)
+    zarr_path = images_dir / zarr_fn
     print(zarr_path)
-    print(f"Array shape: {arr.shape}")
-    print(f"Array dtype: {arr.dtype}")
-    print(f"Array min: {arr[0,0].min()} and max: {arr[0,0].max()}")
 
-    # report micron per pixel on each axis
-    px = f.metadata.channels[0].volume.axesCalibration
-    print(f"The image is {px[0]} micrometers per pixel on Y")
-    print(f"The image is {px[1]} micrometers per pixel on X")
-    
-    write_image(
-        image=arr,
-        group=root,
-        axes="tcyx",
-        scale_factors={}, 
-        coordinate_transformations=[
-            [{"type": "scale", "scale": [1.0, 1.0, px[0], px[1]]}]
-        ],
-    )
-    print("Zarr'd")
+    store = zarr.storage.LocalStore(zarr_path) 
+    root = zarr.group(store, overwrite=True)
 
-z_array = zarr.open('/home/S-DK/TrainTracks/sg100_Well1_1001.zarr', mode='r')
-np_array = np.asarray(z_array['s0'])
+    with nd2.ND2File(nd2_path) as f:
+        arr = f.asarray()
+        print(zarr_path)
+        print(f"Array shape: {arr.shape}")
+        print(f"Array dtype: {arr.dtype}")
+        print(f"Array min: {arr[0,0].min()} and max: {arr[0,0].max()}")
 
-print(f"Zarr shape: {np_array.shape}")
-print(f"Zarr dtype: {np_array.shape}")
-print(f"Array min: {np_array[0,0].min()} and max: {np_array[0,0].max()}")
+        # report micron per pixel on each axis
+        px = f.metadata.channels[0].volume.axesCalibration
+        print(f"The image is {px[0]} micrometers per pixel on Y")
+        print(f"The image is {px[1]} micrometers per pixel on X")
+        
+        write_image(
+            image=arr,
+            group=root,
+            axes="tcyx",
+            scale_factors={}, 
+            coordinate_transformations=[
+                [{"type": "scale", "scale": [1.0, 1.0, px[0], px[1]]}]
+            ],
+        )
+        print("Zarr'd")
