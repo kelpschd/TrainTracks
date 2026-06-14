1c3c9aeafa94298e251b23b03e1db42ed98a8d18
Dan Kelpsch <kelpschd@gmail.com>
2026-06-14 00:05:11 +0000

    Starting flow calc/img norm

---
+++ scripts/optical_flow.py
@@ -0,0 +1,50 @@
+# Calculate flows using:
+# calcOpticalFlowFarneback
+# calcOpticalFlowPyrLK
+
+# mayyyyybe try OpenPIV later:
+# https://openpiv.readthedocs.io/en/latest/
+
+import zarr
+import numpy as np
+from pathlib import Path
+from skimage.exposure import equalize_adapthist
+
+images_dir = Path("/mnt/efs/dl_jrc/student_data/S-DK/Sphere/220725_i11w-hT-M33-I76_sg1035_d10sphere")
+images = list(sorted(images_dir.glob('*.zarr')))
+print(f"Found {len(images)} zarr files")
+
+zarr_root = zarr.open(images[0], mode = 'r')
+arr = zarr_root['s0']
+np_arr = np.array(arr)
+print(np_arr.shape)
+
+# Normalize img using min and max values from the img
+# was i suppose to do this for each frame? I dont think so
+print("Normalizing frames...")
+min_int_val = np_arr.min()
+max_int_val = np_arr.max()
+arr_norm = ((np_arr - min_int_val) / (max_int_val - min_int_val) * 255).astype(np.uint8)
+
+# Get first frame and enhance its contrast
+#         contrast: *enhance_contrast_AHE* - write this one in source utils? 
+print("Computing flow...")
+prev_frame = arr_norm[0] #print me to a png as 'raw'
+
+# Enhance contrast
+# Jen had a rationale for this kernel size (and clip_limit) before but does not 
+# really remember so double check that the output image looks okay
+kernel_size = [8,8] 
+prev_frame = equalize_adapthist(prev_frame, kernel_size=kernel_size, clip_limit=0.01)
+prev_frame = (prev_frame * 255).astype(np.uint8)
+print("The first frame has enhanced contrast")
+
+# write this to a png just to verity that it doesn't look super weird
+
+# Open loop
+#     get next frame, enhance its contrast, and start flow against previous frame 
+#         flow: *calcOpticalFlowFarneback* OR *calcOpticalFlowPyrLK*
+# add empty frame to end of flow time series
+# output zarr
+# write zarr
+
