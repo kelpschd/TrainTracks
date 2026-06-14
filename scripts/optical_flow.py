13bac390eec5abdb0311c65e4079414756b3f50f
Dan Kelpsch <kelpschd@gmail.com>
2026-06-14 13:54:29 +0000

    generated output zarr, working on calcOpticalFlowFarneback

--- scripts/optical_flow.py
+++ scripts/optical_flow.py
@@ -8,6 +8,8 @@
 import zarr
 import numpy as np
 import napari
+import cv2
+from tqdm import tqdm
 from pathlib import Path
 from skimage.exposure import equalize_adapthist
 
@@ -24,6 +26,10 @@ print("Removing unused channels...")
 np_arr = np_arr[:, 0]
 print(np_arr.shape)
 
+print("Generating empty output zarr")
+output_zarr = zarr.zeros(np_arr.shape, dtype=np_arr.dtype, chunks=(1, 2048, 2048))
+print(np.array(output_zarr).shape)
+
 # Normalize img using min and max values from the img
 # was i suppose to do this for each frame? I dont think so
 print("Normalizing frames...")
@@ -34,25 +40,63 @@ arr_norm = ((np_arr - min_int_val) / (max_int_val - min_int_val) * 255).astype(n
 # Get first frame and enhance its contrast
 #         contrast: *enhance_contrast_AHE* - write this one in source utils? 
 print("Computing flow...")
-prev_frame = arr_norm[0] #print me to a png as 'raw'
+prev_frame = arr_norm[0] # check in napari
 
-viewer = napari.Viewer()
-viewer.add_image(prev_frame, name='Raw')
+# viewer = napari.Viewer()
+# viewer.add_image(prev_frame, name='Raw')
 
 # Enhance contrast
 # Jen had a rationale for this kernel size (and clip_limit) before but does not 
 # really remember so double check that the output image looks okay
 # Jen said this looked good for my data - the orginal was 8x8 and we can change back to that if needed
 kernel_size = [16,16] 
-contrast_test = equalize_adapthist(prev_frame, kernel_size=kernel_size, clip_limit=0.01)
-contrast_test = (contrast_test * 255).astype(np.uint8) #print me to a png as 'enhanced'
+prev_frame_contrast = equalize_adapthist(prev_frame, kernel_size=kernel_size, clip_limit=0.01)
+prev_frame_contrast = (prev_frame_contrast * 255).astype(np.uint8) #print me to a png as 'enhanced'
 print("The first frame has enhanced contrast")
-viewer.add_image(contrast_test, name='Enhanced 16x16 0.01')
+#viewer.add_image(contrast_test, name='Enhanced 16x16 0.01')
+#napari.run()
+
+# Set up for previous flow
+prev_flow = None
 
 
 
+# open loop to iterate through each frame of the time series
+for i in tqdm(range(1, arr_norm.shape[0]), desc = "Computing optical flow"):
+    curr_frame = arr_norm[i]
 
-napari.run()
+    # normalize current_frame
+    kernel_size = [16,16] 
+    curr_frame_contrast = equalize_adapthist(curr_frame, kernel_size=kernel_size, clip_limit=0.01)
+    curr_frame_contrast = (curr_frame_contrast * 255).astype(np.uint8)
+
+    # Calculate flow
+    # These are the settings that we set up prevously but it would be adventageous 
+    # to take the values from a config file
+    flow = cv2.calcOpticalFlowFarneback(
+        prev=prev_frame_contrast,
+        next=curr_frame_contrast,
+        prev_flow = None, 
+        pyr_sclae = 0.6,
+        levels=5,
+        winsize=15,
+        iterations=8,
+        poly_n=3,
+        poly_sigma=0.7,
+        flags=cv2.OPTFLOW_FARNEBACK_GAUSSIAN
+        )
+
+    # Goal is to also smooth the flow as we calculate it
+    # Calculated using 30% of the flow from the prevous frame
+    if prev_flow is not None: 
+        temporal_smoothing_sigma = 0.7 
+        flow = temporal_smoothing_sigma * flow + (1 - temporal_smoothing_sigma) * prev_flow
+
+    prev_frame = curr_frame
+    prev_flow = flow
+
+# There is some frame averaging with Jens script, but we can remove that
+
 # Open loop
 #     get next frame, enhance its contrast, and start flow against previous frame 
 #         flow: *calcOpticalFlowFarneback* OR *calcOpticalFlowPyrLK*
