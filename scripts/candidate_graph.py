198bb58d5145b1e3a4cfc1f197cfd7aa475089bb
Dan Kelpsch <kelpschd@gmail.com>
2026-06-15 14:19:07 +0000

    I dont really need this in np, so ignore that. iterate through df and get blob detections > nodes, validate with visualization via napari

--- scripts/candidate_graph.py
+++ scripts/candidate_graph.py
@@ -1,50 +1,58 @@
+import napari
+import zarr
+
+from pathlib import Path
 import numpy as np
 import pandas as pd
+import networkx as nx
 
 # Load in detected spots
 # csv -> numpy array
 blobs_fp = "/mnt/efs/dl_jrc/student_data/S-DK/Sphere/220725_i11w-hT-M33-I76_sg1035_d10sphere/sg100_Well5_1018_blobs.csv"
 blobs_df = pd.read_csv(blobs_fp)
 print(blobs_df)
-blobs_np = blobs_df.to_numpy()
-print(blobs_np)
+print(blobs_df.head())
+print(blobs_df.columns)
+# blobs_np = blobs_df.to_numpy()
+print("Annotated blobs loaded!")
 
-# Generate nodes from "segmentation" - remember this will have to handle blob coords instead of segmentations
-# And build candidate graph
-def nodes_from_segmentation(segmentation: np.ndarray) -> nx.DiGraph:
-    """Extract candidate nodes from a segmentation.
+cand_graph = nx.DiGraph()
+for idx, row in blobs_df.iterrows():
+    attrs = {
+        "t": row["frame"],
+        "x": row["x"],
+        "y": row["y"],
+    }
+    cand_graph.add_node(idx, **attrs)
 
-    Args:
-        segmentation (np.ndarray): A numpy array with integer labels and dimensions
-            (t, y, x).
+print(cand_graph)
 
-    Returns:
-        nx.DiGraph: A candidate graph with only nodes.
-    """
-    cand_graph = nx.DiGraph()
-    print("Extracting nodes from segmentation")
-    for t in tqdm(range(len(segmentation))):
-        seg_frame = segmentation[t]
-        props = skimage.measure.regionprops(seg_frame)
-        for regionprop in props:
-            node_id = regionprop.label
-            x = float(regionprop.centroid[0])
-            y = float(regionprop.centroid[1])
-            attrs = {
-                "t": t,
-                "x": x,
-                "y": y,
-                "score": float(probabilities[t, int(x // 2), int(y // 2)]),
-            }
-            assert node_id not in cand_graph.nodes
-            cand_graph.add_node(node_id, **attrs)
-    return cand_graph
+# check to see if nodes and blobs align
+# import raw img for reference
+images_dir = Path("/mnt/efs/dl_jrc/student_data/S-DK/Sphere/220725_i11w-hT-M33-I76_sg1035_d10sphere")
+images = list(sorted(images_dir.glob('*.zarr')))
+print(f"Found {len(images)} zarr files")
+img_num = 9
+print(images[img_num].name)
+zarr_root = zarr.open(images[img_num], mode = 'r')
+arr = zarr_root['s0']
+np_arr = np.array(arr)
+print(np_arr.shape)
 
-cand_graph = nodes_from_segmentation(segmentation)
-
-points_array = np.array([[data["t"], data["x"], data["y"]] for node, data in cand_graph.nodes(data=True)])
+# set up cand_graph to be viewed in napari - remember my images are TYX 
+print(cand_graph.nodes[0])
+points_array = np.array([[data["t"], data["y"], data["x"]] for node, data in cand_graph.nodes(data=True)])
+print(points_array[0:5])
 cand_points_layer = napari.layers.Points(data=points_array, name="cand_points")
+
+# check everything in napari - candidate nodes should be in the center of the detected blobs (red circles)
+viewer = napari.Viewer()
+viewer.add_image(np_arr[:,0], name = "Raw image")
+viewer.add_points(blobs_df, size = 30, face_color = "transparent", border_color="red", border_width=0.1)
 viewer.add_layer(cand_points_layer)
+napari.run()
+
+
 
 # Add edges
 def _compute_node_frame_dict(cand_graph: nx.DiGraph) -> dict[int, list[Any]]:
@@ -104,10 +112,10 @@ def add_cand_edges(
         prev_node_ids = next_node_ids
         prev_kdtree = next_kdtree
 
-add_cand_edges(cand_graph, max_edge_distance=50)
+# add_cand_edges(cand_graph, max_edge_distance=50)
 
-print(f"Our candidate graph has {cand_graph.number_of_nodes()} nodes and {cand_graph.number_of_edges()} edges")
-print(f"Our ground truth track graph has {gt_tracks.number_of_nodes()} nodes and {gt_tracks.number_of_edges()}")
+# print(f"Our candidate graph has {cand_graph.number_of_nodes()} nodes and {cand_graph.number_of_edges()} edges")
+# print(f"Our ground truth track graph has {gt_tracks.number_of_nodes()} nodes and {gt_tracks.number_of_edges()}")
 
 # Calculate optical flow feature to incorporate into the weight cost
 # set up optimization problem
