3b52dbb53e9e135dd336e992650f5b223dbb574d
Dan Kelpsch <kelpschd@gmail.com>
2026-06-15 13:15:13 +0000

    Starting efforts on loading in identified blobs from an image and building out the candidate graph (most code copied from exercise)

---
+++ scripts/candidate_graph.py
@@ -0,0 +1,112 @@
+import numpy as np
+import pandas as pd
+
+# Load in detected spots
+# csv -> numpy array
+blobs_fp = "/mnt/efs/dl_jrc/student_data/S-DK/Sphere/220725_i11w-hT-M33-I76_sg1035_d10sphere/sg100_Well5_1018_blobs.csv"
+blobs = pd.read_csv(blobs_fp)
+print(blobs)
+
+# Generate nodes from "segmentation" - remember this will have to handle blob coords instead of segmentations
+# And build candidate graph
+def nodes_from_segmentation(segmentation: np.ndarray) -> nx.DiGraph:
+    """Extract candidate nodes from a segmentation.
+
+    Args:
+        segmentation (np.ndarray): A numpy array with integer labels and dimensions
+            (t, y, x).
+
+    Returns:
+        nx.DiGraph: A candidate graph with only nodes.
+    """
+    cand_graph = nx.DiGraph()
+    print("Extracting nodes from segmentation")
+    for t in tqdm(range(len(segmentation))):
+        seg_frame = segmentation[t]
+        props = skimage.measure.regionprops(seg_frame)
+        for regionprop in props:
+            node_id = regionprop.label
+            x = float(regionprop.centroid[0])
+            y = float(regionprop.centroid[1])
+            attrs = {
+                "t": t,
+                "x": x,
+                "y": y,
+                "score": float(probabilities[t, int(x // 2), int(y // 2)]),
+            }
+            assert node_id not in cand_graph.nodes
+            cand_graph.add_node(node_id, **attrs)
+    return cand_graph
+
+cand_graph = nodes_from_segmentation(segmentation)
+
+points_array = np.array([[data["t"], data["x"], data["y"]] for node, data in cand_graph.nodes(data=True)])
+cand_points_layer = napari.layers.Points(data=points_array, name="cand_points")
+viewer.add_layer(cand_points_layer)
+
+# Add edges
+def _compute_node_frame_dict(cand_graph: nx.DiGraph) -> dict[int, list[Any]]:
+    """Compute dictionary from time frames to node ids for candidate graph.
+
+    Args:
+        cand_graph (nx.DiGraph): A networkx graph
+
+    Returns:
+        dict[int, list[Any]]: A mapping from time frames to lists of node ids.
+    """
+    node_frame_dict: dict[int, list[Any]] = {}
+    for node, data in cand_graph.nodes(data=True):
+        t = data["t"]
+        if t not in node_frame_dict:
+            node_frame_dict[t] = []
+        node_frame_dict[t].append(node)
+    return node_frame_dict
+
+def create_kdtree(cand_graph: nx.DiGraph, node_ids: Iterable[Any]) -> scipy.spatial.KDTree:
+    positions = [[cand_graph.nodes[node]["x"], cand_graph.nodes[node]["y"]] for node in node_ids]
+    return scipy.spatial.KDTree(positions)
+
+def add_cand_edges(
+    cand_graph: nx.DiGraph,
+    max_edge_distance: float,
+) -> None:
+    """Add candidate edges to a candidate graph by connecting all nodes in adjacent
+    frames that are closer than max_edge_distance. Also adds attributes to the edges.
+
+    Args:
+        cand_graph (nx.DiGraph): Candidate graph with only nodes populated. Will
+            be modified in-place to add edges.
+        max_edge_distance (float): Maximum distance that objects can travel between
+            frames. All nodes within this distance in adjacent frames will by connected
+            with a candidate edge.
+    """
+    print("Extracting candidate edges")
+    node_frame_dict = _compute_node_frame_dict(cand_graph)
+
+    frames = sorted(node_frame_dict.keys())
+    prev_node_ids = node_frame_dict[frames[0]]
+    prev_kdtree = create_kdtree(cand_graph, prev_node_ids)
+    for frame in tqdm(frames):
+        if frame + 1 not in node_frame_dict:
+            continue
+        next_node_ids = node_frame_dict[frame + 1]
+        next_kdtree = create_kdtree(cand_graph, next_node_ids)
+
+        matched_indices = prev_kdtree.query_ball_tree(next_kdtree, max_edge_distance)
+
+        for prev_node_id, next_node_indices in zip(prev_node_ids, matched_indices):
+            for next_node_index in next_node_indices:
+                next_node_id = next_node_ids[next_node_index]
+                cand_graph.add_edge(prev_node_id, next_node_id)
+
+        prev_node_ids = next_node_ids
+        prev_kdtree = next_kdtree
+
+add_cand_edges(cand_graph, max_edge_distance=50)
+
+print(f"Our candidate graph has {cand_graph.number_of_nodes()} nodes and {cand_graph.number_of_edges()} edges")
+print(f"Our ground truth track graph has {gt_tracks.number_of_nodes()} nodes and {gt_tracks.number_of_edges()}")
+
+# Calculate optical flow feature to incorporate into the weight cost
+# set up optimization problem
+
