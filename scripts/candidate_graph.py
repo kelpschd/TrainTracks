fixes to previous approach to pseudosegmentation and average flow calc

scripts/candidate_graph.py
@@ -10,61 +10,34 @@ import networkx as nx
 from pathlib import Path
 from tqdm.auto import tqdm
 from typing import Iterable, Any
-from skimage.draw import disk
+from skimage.morphology import disk, binary_dilation
+from geff import write
 
 # Load in detected spots
 # csv -> numpy array
 blobs_fp = "/mnt/efs/dl_jrc/student_data/S-DK/Sphere/220725_i11w-hT-M33-I76_sg1035_d10sphere/sg100_Well5_1018_blobs.csv"
 blobs_df = pd.read_csv(blobs_fp)
-test = blobs_df.iloc[0]
-print(test['frame'])
-# blobs_np = blobs_df.to_numpy()
+print(blobs_df)
+blobs_np = blobs_df.to_numpy().astype(np.uint16)
+print(blobs_np)
 print("Annotated blobs loaded!")
 
 # import flow zarr
 flow_path = Path("/home/S-DK/TrainTracks/flow.zarr")
 flow_root = zarr.open_group(flow_path, mode='r')
 flow_raw = flow_root['flow_raw'] 
+flow_arr = np.array(flow_raw)
+print(flow_arr.dtype)
+print(flow_arr.shape)
+print(type(flow_arr))
 
-# define a region around the ROIs
-def calculate_flow_offset_pseudomask(
-        row: pd.Series, 
-        flow_raw: np.ndarray, 
-        radius=5,
-):
-    # input is: row (that has frame, x, and y) as well as the flow zarr, and a radius
-    t = int(row['frame'])
-    y = int(row['y'])
-    x = int(row['x'])
+mask = np.zeros(flow_raw.shape[:-1], dtype = np.uint16)
+mask[blobs_np[:, 0], blobs_np[:, 1], blobs_np[:, 2]] = True
 
-    flow_frame = np.array(flow_raw[t])
-
-    rr, cc = disk((y, x), radius, shape=flow_frame.shape[:2]) 
-
-    flow_x = flow_frame[rr, cc, 0].mean()
-    flow_y = flow_frame[rr, cc, 1].mean()
-
-    return flow_x, flow_y
-    # pull flows in a region (i.e. a small circle around our detected blob) and average them
-
-    # use skimage.morphology.disk to make a roi around the centroid and then calcuate the average flow in that space
-
-# Generate cand_graph 
-# Generate it with flow attached
-cand_graph = nx.DiGraph()
-for idx, row in tqdm(blobs_df.iterrows(), total=len(blobs_df), desc="Building candidate graph"):
-    flow = calculate_flow_offset_pseudomask(row, flow_raw, 5)
-    # take flow zarr and slice for each time point (frame) basically just bring in the whole row to the function
-    # flow = # function goes here
-    attrs = {
-        "t": int(row["frame"]),
-        "x": row["x"],
-        "y": row["y"],
-        "flow": flow,
-    }
-    cand_graph.add_node(idx, **attrs)
-print(cand_graph)
-nx.write_graphml(cand_graph, 'cand_graph.graphml')
+radius = 5 # will consider making this bigger to get a nicer average flow
+dilated = np.zeros(mask.shape, dtype = np.uint16)
+for frame in range(0, mask.shape[0]):
+    dilated[frame] = binary_dilation(mask[frame], footprint = disk(radius))
 
 # Check to see if nodes and blobs align
 # import raw img for reference
@@ -77,15 +50,73 @@ np_arr = np.array(arr)
 print("Raw image loaded!")
 
 # set up cand_graph to be viewed in napari - remember my images are TYX 
+# points_array = np.array([[data["t"], data["y"], data["x"]] for node, data in cand_graph.nodes(data=True)])
+# cand_points_layer = napari.layers.Points(data=points_array, name="cand_points")
+
+# Generate cand_graph 
+# Generate it with flow attached
+cand_graph = nx.DiGraph()
+last_node_id = 0
+
+for frame in range(0, dilated.shape[0]): 
+    segmentation = skimage.measure.label(dilated[frame])
+    flow_frame = flow_arr[frame]
+    props = skimage.measure.regionprops(segmentation)
+    last_node_id += len(props)
+    
+    for regionprop in props:
+        node_id = int(regionprop.label)
+        region = segmentation[frame] == node_id 
+        centroid = (float(regionprop.centroid[0]),
+                    float(regionprop.centroid[1]))
+        flow = (float(np.mean(flow_frame[region][:1])),
+                float(np.mean(flow_frame[region][:0])))
+        attrs = {
+            "t": frame,
+            "x": centroid[1],
+            "y": centroid[0],
+            "flow": flow,
+        }
+
+        node_id += int(last_node_id)
+        print(node_id)
+        cand_graph.add_node(node_id, **attrs)
+print(cand_graph)
+
+
+quit()
+
+for idx, row in tqdm(blobs_df.iterrows(), total=len(blobs_df), desc="Building candidate graph"):
+    flow = calculate_flow_offset_pseudomask(row, flow_raw, 5)
+    # take flow zarr and slice for each time point (frame) basically just bring in the whole row to the function
+    # flow = # function goes here
+    attrs = {
+        "t": int(row["frame"]),
+        "x": row["x"],
+        "y": row["y"],
+        "flow": flow,
+    }
+    cand_graph.add_node(idx, **attrs)
+print(cand_graph)
+# nx.write_graphml(cand_graph, 'cand_graph.graphml') # doesn't like the flow tuple...
+# jen says we can write this to geff
+write(
+    cand_graph,
+    "cand_graph.geff",
+    zarr_format=2  # 2 or 3
+)
+
+# set up cand_graph to be viewed in napari - remember my images are TYX 
 points_array = np.array([[data["t"], data["y"], data["x"]] for node, data in cand_graph.nodes(data=True)])
 cand_points_layer = napari.layers.Points(data=points_array, name="cand_points")
 
 # check everything in napari - candidate nodes should be in the center of the detected blobs (red circles)
-# viewer = napari.Viewer()
-# viewer.add_image(np_arr[:,0], name = "Raw image")
-# viewer.add_points(blobs_df, size = 30, face_color = "transparent", border_color="red", border_width=0.1)
-# viewer.add_layer(cand_points_layer)
-# napari.run()
+viewer = napari.Viewer()
+viewer.add_image(np_arr[:,0], name = "Raw image")
+viewer.add_points(blobs_df, size = 30, face_color = "transparent", border_color="red", border_width=0.1)
+viewer.add_labels(dilated)
+viewer.add_layer(cand_points_layer)
+napari.run()
 
 # Add edges
 def _compute_node_frame_dict(cand_graph: nx.DiGraph) -> dict[int, list[Any]]: