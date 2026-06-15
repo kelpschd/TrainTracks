d0d2652f3aa1774050d3af11005f0f8c48b6bdc6
Dan Kelpsch <kelpschd@gmail.com>
2026-06-15 15:01:57 +0000

    Dropping prints I don't need and pseudocode for flow_offset calculation

--- scripts/candidate_graph.py
+++ scripts/candidate_graph.py
@@ -1,25 +1,34 @@
 import napari
 import zarr
+import scipy
 
 from pathlib import Path
 import numpy as np
 import pandas as pd
 import networkx as nx
 
+from tqdm.auto import tqdm
+from typing import Iterable, Any
+
 # Load in detected spots
 # csv -> numpy array
 blobs_fp = "/mnt/efs/dl_jrc/student_data/S-DK/Sphere/220725_i11w-hT-M33-I76_sg1035_d10sphere/sg100_Well5_1018_blobs.csv"
 blobs_df = pd.read_csv(blobs_fp)
-print(blobs_df)
-print(blobs_df.head())
-print(blobs_df.columns)
 # blobs_np = blobs_df.to_numpy()
 print("Annotated blobs loaded!")
+
+# import flow zarr
+
+
+def calculate_flow_offset():
+    # take flow zarr info
+    # pull flows in a region (i.e. a small circle around our detected blob) and average them
+    pass
 
 cand_graph = nx.DiGraph()
 for idx, row in blobs_df.iterrows():
     attrs = {
-        "t": row["frame"],
+        "t": int(row["frame"]),
         "x": row["x"],
         "y": row["y"],
     }
@@ -27,32 +36,26 @@ for idx, row in blobs_df.iterrows():
 
 print(cand_graph)
 
-# check to see if nodes and blobs align
+# Check to see if nodes and blobs align
 # import raw img for reference
 images_dir = Path("/mnt/efs/dl_jrc/student_data/S-DK/Sphere/220725_i11w-hT-M33-I76_sg1035_d10sphere")
 images = list(sorted(images_dir.glob('*.zarr')))
-print(f"Found {len(images)} zarr files")
 img_num = 9
-print(images[img_num].name)
 zarr_root = zarr.open(images[img_num], mode = 'r')
 arr = zarr_root['s0']
 np_arr = np.array(arr)
-print(np_arr.shape)
+print("Raw image loaded!")
 
 # set up cand_graph to be viewed in napari - remember my images are TYX 
-print(cand_graph.nodes[0])
 points_array = np.array([[data["t"], data["y"], data["x"]] for node, data in cand_graph.nodes(data=True)])
-print(points_array[0:5])
 cand_points_layer = napari.layers.Points(data=points_array, name="cand_points")
 
 # check everything in napari - candidate nodes should be in the center of the detected blobs (red circles)
-viewer = napari.Viewer()
-viewer.add_image(np_arr[:,0], name = "Raw image")
-viewer.add_points(blobs_df, size = 30, face_color = "transparent", border_color="red", border_width=0.1)
-viewer.add_layer(cand_points_layer)
-napari.run()
-
-
+# viewer = napari.Viewer()
+# viewer.add_image(np_arr[:,0], name = "Raw image")
+# viewer.add_points(blobs_df, size = 30, face_color = "transparent", border_color="red", border_width=0.1)
+# viewer.add_layer(cand_points_layer)
+# napari.run()
 
 # Add edges
 def _compute_node_frame_dict(cand_graph: nx.DiGraph) -> dict[int, list[Any]]:
@@ -97,9 +100,11 @@ def add_cand_edges(
     prev_node_ids = node_frame_dict[frames[0]]
     prev_kdtree = create_kdtree(cand_graph, prev_node_ids)
     for frame in tqdm(frames):
+        #print(f"Current frame: {frame+1}")
         if frame + 1 not in node_frame_dict:
             continue
         next_node_ids = node_frame_dict[frame + 1]
+        # print(f"Length of next node ids: {len(next_node_ids)}")
         next_kdtree = create_kdtree(cand_graph, next_node_ids)
 
         matched_indices = prev_kdtree.query_ball_tree(next_kdtree, max_edge_distance)
@@ -107,16 +112,16 @@ def add_cand_edges(
         for prev_node_id, next_node_indices in zip(prev_node_ids, matched_indices):
             for next_node_index in next_node_indices:
                 next_node_id = next_node_ids[next_node_index]
+                # print(f"Candidate edge: [{prev_node_id}, {next_node_id}]")
                 cand_graph.add_edge(prev_node_id, next_node_id)
 
         prev_node_ids = next_node_ids
         prev_kdtree = next_kdtree
 
-# add_cand_edges(cand_graph, max_edge_distance=50)
-
-# print(f"Our candidate graph has {cand_graph.number_of_nodes()} nodes and {cand_graph.number_of_edges()} edges")
-# print(f"Our ground truth track graph has {gt_tracks.number_of_nodes()} nodes and {gt_tracks.number_of_edges()}")
-
-# Calculate optical flow feature to incorporate into the weight cost
-# set up optimization problem
-
+print("Finding candidate edges")
+add_cand_edges(cand_graph, max_edge_distance=30)
+print(f"Our candidate graph has {cand_graph.number_of_nodes()} nodes and {cand_graph.number_of_edges()} edges")
+# this is much fewer edges than we really expected but it might be 
+# related to how much flashing I see in blob detection, we may try
+# to add skip connections - will have to see if there is code avaialbe
+# for that already
