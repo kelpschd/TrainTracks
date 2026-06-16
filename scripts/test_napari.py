de699c28d5009f3b4d451fa25f26739f45e2e5f9
Dan Kelpsch <kelpschd@gmail.com>
2026-06-16 14:58:52 +0000

    package testing

--- scripts/test_napari.py
+++ scripts/test_napari.py
@@ -9,7 +9,7 @@ import numpy as np
 from funtracks.import_export import import_from_geff
 # from motile_tracker.motile.backend import MotileRun
 # from motile_tracker.data_views.views.tree_view.tree_widget import TreeWidget
-from motile_tracker.data_views.views_coordinator.tracks_viewer import TracksViewer
+# from motile_tracker.data_views.views_coordinator.tracks_viewer import TracksViewer
 
 # currently have a solution graph in GEFF and the raw image
 # want to visualize solution graph tracks
